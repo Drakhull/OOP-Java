e1822e3fabfafc1f84b47ae092b46764f8a5b816
Drakhuçll <Iuri@Daedric.localdomain>
2024-11-28 21:20:01 -0300

    Class Product prototype

---
+++ Product.java
@@ -0,0 +1,22 @@
+import java.math.BigDecimal;
+
+public class Product {
+    private static int idCounter;
+
+    private int id;
+    private int quantity;
+    private String name;
+    private String description;
+    private String category;
+    private BigDecimal price;
+
+    public Product(int quantity, String name, String description,
+                   String category, String price) {
+        this.id = idCounter++;
+        this.quantity = quantity;
+        this.name = name;
+        this.description = description;
+        this.category = category;
+        this.price = new BigDecimal(price);
+    }
+}
