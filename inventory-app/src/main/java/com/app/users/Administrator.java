eed2bd9df3ec036bf01ec6569a3341307c29c699
Iuri ⚰️ <141394893+Drakhull@users.noreply.github.com>
2024-12-12 09:56:37 -0300

    Delete inventory-app directory

--- inventory-app/src/main/java/com/app/users/Administrator.java
+++
@@ -1,8 +0,0 @@
-package com.app.users;
-
-public class Administrator extends User{
-    public Administrator (String name, String email, String password)
-        throws Exception {
-        super(name, email, password);
-    }
-}
